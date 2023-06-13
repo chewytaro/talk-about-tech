3deb564b8fba70a38fd5200b72d5918a2091ef38
chewytaro <117787401+chewytaro@users.noreply.github.com>
2023-06-13 01:51:43 -0700

    updated port

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+var Sequelize = require('sequelize');
 
 require('dotenv').config();
 
@@ -10,9 +10,8 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: process.env.PORT || 3001
+    host: 'localhost',
+    dialect: 'mysql'
   });
 }
 
