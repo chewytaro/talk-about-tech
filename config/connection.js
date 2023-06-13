7243da4dc90a0e7dd528ea26ad910474ae3de08c
chewytaro <117787401+chewytaro@users.noreply.github.com>
2023-06-13 02:26:28 -0700

    updated packages

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: 'localhost',
     dialect: 'mysql',
-    port: '3306'
+    port: 3306
   });
 }
 
