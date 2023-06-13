bfba3cf84426c82de14ddafecedd7ecab4a11138
chewytaro <117787401+chewytaro@users.noreply.github.com>
2023-06-13 02:13:03 -0700

    changed port

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,8 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: '3306'
   });
 }
 
